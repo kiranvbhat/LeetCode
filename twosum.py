cc1d53f0e4dabb2fc15d03dd66a9ae92f0ac6c6d
kiranvbhat <76144656+kiranvbhat@users.noreply.github.com>
2022-09-02 17:16:57 -0700

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    # O(n) time, O(n) space
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_to_indices = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            comp = target - num
+            if comp in nums_to_indices:
+                return [nums_to_indices[comp], i]
+            nums_to_indices[num] = i
+        return []

c1f18f5dcb0dec54fa87e61d45cfb4637e37d106
kiranvbhat <76144656+kiranvbhat@users.noreply.github.com>
2022-09-04 01:02:14 -0700

    Create 104_MaxDepthOfBinaryTree.cpp

---
+++ 104_MaxDepthOfBinaryTree.cpp
@@ -0,0 +1,44 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // RECURSIVE DFS
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+
+// ------------------------------------------------------------------------------------------    
+    
+    // ITERATIVE BFS
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int depth = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode* cur = q.front();
+                q.pop();
+                if (cur->left != nullptr) q.push(cur->left);
+                if (cur->right != nullptr) q.push(cur->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};

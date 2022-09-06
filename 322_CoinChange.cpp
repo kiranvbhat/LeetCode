2dae8363bc16e21005bd8f0fd39ef3c50c32f498
kiranvbhat <76144656+kiranvbhat@users.noreply.github.com>
2022-09-05 23:13:07 -0700

    Create 322_CoinChange.cpp

---
+++ 322_CoinChange.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/coin-change/
+
+class Solution {
+public:
+    // O(n * amount) time, bottom-up DP solution
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> fewestFromAmount(amount + 1);
+        fewestFromAmount[0] = 0;
+        for (int amt = 1; amt < amount + 1; amt++) {
+            int minCoins = 100000;  // acts as infinity, since amount is constrained to <= 10^4
+            for (int coin : coins) {
+                if (amt - coin >= 0) {
+                    minCoins = min(minCoins, 1 + fewestFromAmount[amt - coin]);
+                }
+            }
+            fewestFromAmount[amt] = minCoins;
+        }
+        int fewest = fewestFromAmount[amount];
+        return (fewest != 100000) ? fewest : -1;
+    }
+};
